38216c0a91d8426aa7e7895e645c63a2399aae06
BeaMoura0906 <bea-moura@outlook.fr>
2024-04-24 10:46:35 +0200

    Update sql file

--- db/worga.sql
+++ db/worga.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 16 avr. 2024 à 16:05
+-- Généré le : mer. 24 avr. 2024 à 08:42
 -- Version du serveur : 8.2.0
--- Version de PHP : 7.4.33
+-- Version de PHP : 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,15 +32,15 @@ USE `worga`;
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE IF NOT EXISTS `accounts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `estimates_total` double NOT NULL,
-  `invoices_total` double NOT NULL,
-  `receipts_total` double NOT NULL,
-  `rest_to_invoice` double NOT NULL,
-  `rest_to_cash` double NOT NULL,
+  `estimates_total` decimal(10,2) NOT NULL,
+  `invoices_total` decimal(10,2) NOT NULL,
+  `receipts_total` decimal(10,2) NOT NULL,
+  `rest_to_invoice` decimal(10,2) NOT NULL,
+  `rest_to_cash` decimal(10,2) NOT NULL,
   `client_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_account_id_client` (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -51,17 +51,25 @@ CREATE TABLE IF NOT EXISTS `accounts` (
 DROP TABLE IF EXISTS `clients`;
 CREATE TABLE IF NOT EXISTS `clients` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` int NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `other` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `inserted_at` int NOT NULL,
-  `updated_at` int NOT NULL,
+  `inserted_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
   `user_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_client_id_user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `clients`
+--
+
+INSERT INTO `clients` (`id`, `last_name`, `first_name`, `address`, `phone`, `email`, `other`, `inserted_at`, `updated_at`, `user_id`) VALUES
+(1, 'Dupond', 'Jean', '1 rue Rivoli 99000 Ville', 623456789, 'jean.dupond@test.com', '', '2024-04-22 00:00:00', '2024-04-22 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -77,10 +85,11 @@ CREATE TABLE IF NOT EXISTS `documents` (
   `path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `inserted_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
-  `transaction_id` int NOT NULL,
+  `financial_transaction_id` int NOT NULL,
   `user_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_doc_id_ft` (`transaction_id`)
+  KEY `idx_doc_id_ft` (`financial_transaction_id`),
+  KEY `idx_doc_id_user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -95,14 +104,15 @@ CREATE TABLE IF NOT EXISTS `financial_transactions` (
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `category` enum('to_be_debited','debit','credit') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `amount_ex_vat` double NOT NULL,
-  `vat_rate` int NOT NULL,
+  `amount_ex_vat` decimal(10,2) NOT NULL,
+  `vat_rate` decimal(5,2) NOT NULL,
   `inserted_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `account_id` int NOT NULL,
   `user_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_ft_id_account` (`account_id`)
+  KEY `idx_ft_id_account` (`account_id`),
+  KEY `idx_ft_id_user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -117,8 +127,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `login` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` enum('admin','editor','visitor','') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` date NOT NULL,
-  `updated_at` date NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
   `is_active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`)
@@ -129,9 +139,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `role`, `created_at`, `updated_at`, `is_active`) VALUES
-(1, 'admin', '$argon2id$v=19$m=65536,t=2,p=1$ReP2U85vtpuVglT22qKSKg$jE9ExXAsUXsF+QCyzckM4P6Is/6zoUZgkFu0bxzuKbc', 'admin', '2024-03-24', '2024-04-16', 1),
-(2, 'editor', '$argon2id$v=19$m=65536,t=2,p=1$IIHtPeTazpcLAmCL8DE8Ag$xxw9qahxXMYgykISk09MpfFDPh3hmr8bRagUFeDRkgo', 'editor', '2024-04-04', '2024-04-16', 1),
-(3, 'visitor', '$argon2id$v=19$m=65536,t=2,p=1$MyMgsNzRP390IKbZ2C63gg$D2gbsnppFlFyjX19kCOzt7sp57g6XyCh97/Qrhq1NgI', 'visitor', '2024-04-16', '2024-04-16', 0);
+(1, 'admin', '$argon2id$v=19$m=65536,t=2,p=1$ReP2U85vtpuVglT22qKSKg$jE9ExXAsUXsF+QCyzckM4P6Is/6zoUZgkFu0bxzuKbc', 'admin', '2024-03-24 00:00:00', '2024-04-16 00:00:00', 1),
+(2, 'editor', '$argon2id$v=19$m=65536,t=2,p=1$IIHtPeTazpcLAmCL8DE8Ag$xxw9qahxXMYgykISk09MpfFDPh3hmr8bRagUFeDRkgo', 'editor', '2024-04-04 00:00:00', '2024-04-16 00:00:00', 1),
+(3, 'visitor', '$argon2id$v=19$m=65536,t=2,p=1$MyMgsNzRP390IKbZ2C63gg$D2gbsnppFlFyjX19kCOzt7sp57g6XyCh97/Qrhq1NgI', 'visitor', '2024-04-16 00:00:00', '2024-04-16 00:00:00', 0);
 
 --
 -- Contraintes pour les tables déchargées
@@ -153,13 +163,15 @@ ALTER TABLE `clients`
 -- Contraintes pour la table `documents`
 --
 ALTER TABLE `documents`
-  ADD CONSTRAINT `idx_doc_id_ft` FOREIGN KEY (`transaction_id`) REFERENCES `financial_transactions` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `idx_doc_id_ft` FOREIGN KEY (`financial_transaction_id`) REFERENCES `financial_transactions` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `idx_doc_id_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `financial_transactions`
 --
 ALTER TABLE `financial_transactions`
-  ADD CONSTRAINT `idx_ft_id_account` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `idx_ft_id_account` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `idx_ft_id_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
